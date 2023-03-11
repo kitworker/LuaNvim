5d1bf6428ec07746904cb79c9dbc405671ac2130
kitworker <kasyanov.nn@gmail.com>
2023-03-11 09:26:35 -0500

    jobsend to vsplit terminal

--- test1.lua
+++ test1.lua
@@ -3,6 +3,9 @@ local function run_jest()
   print('Hellow form function')
   local file = vim.fn.expand("%:p")
   print("my file id " .. file)
+  vim.cmd("vsplit | terminal ")
+  local command = ':call jobsend(b:terminal_job_id, "echo hello world\\n")'
+  vim.cmd(command)
 end
 
 return {
